a00ebc14368147326f6f8788ae697f2405f2595c
tanveer <mdtanviralamht731@gmail.com>
2022-03-22 23:16:22 +0600

    cart btn added

--- src/components/Shop/Product/Product.js
+++ src/components/Shop/Product/Product.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {name, price, ratings,img}= props.product
+    const {name, price, ratings,img,seller}= props.product
     return (
         <div className='product' >
             <img src={img} alt="products" />
             <div className="product-info">
                 <p className='product-name'>{name}</p>
                 <p className='product-price'>Price: ${price}</p>
-                <p className='product-rating'>Ratings: {ratings} Star</p>
+                <p className='product-manuf'>Manufacturer: {seller}</p>
+                <p className='product-price'>Ratings: {ratings} Star</p>
             </div>
+            <button className='product-btn'>
+            <span>Add to Cart</span>
+            </button>
         </div>
     );
 };
