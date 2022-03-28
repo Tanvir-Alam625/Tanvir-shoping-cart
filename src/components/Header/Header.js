7c99f26183784ae5ad6ab985c76110e5960317f0
tanveer <mdtanviralamht731@gmail.com>
2022-03-29 03:01:47 +0600

    header responsived

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,38 @@
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import Logo from "../../images/Logo.svg";
 import Link from "../Link/Link";
 const Header = () => {
   const [links, setLinks] = useState([]);
+  const [toggle, setToggle] = useState(false);
   useState(() => {
     fetch("link.json")
       .then((response) => response.json())
       .then((data) => setLinks(data));
   }, []);
+  const togglerFunction = () => setToggle(true);
   return (
     <div className="relative">
-      <nav className=" h-[80px] bg-[#1c2b35] flex justify-between py-0 px-[140px] items-center">
-        <img src={Logo} alt="nav-logo" />
-        <div className="">
+      <nav className=" h-[80px]  flex px-0 justify-between py-0 lg:px-[140px] items-center bg-[#1c2b35]">
+        <img src={Logo} alt="nav-logo" className="z-20 pl-6 lg:p-0" />
+        <div
+          className={`
+          flex flex-col lg:flex-row lg:relative lg:mt-0 absolute  pt-12 lg:pt-0 z-10  w-full lg:w-auto bg-[#1c2b35]
+          ${toggle ? "mt-[220px]" : "mt-[-300px]"}
+          `}
+        >
           {links.map((link) => (
             <Link data={link} key={link.id} />
           ))}
         </div>
+
+        <FontAwesomeIcon
+          icon={toggle ? faXmark : faBars}
+          onClick={() => setToggle(!toggle)}
+          className=" text-white  visible lg:hidden  my-6 w-[50px] h-[50px] mr-6 z-40 cursor-pointer duration-500 ease-in  font-light "
+        />
+        {/* <FontAwesomeIcon icon={faXmark}/> */}
       </nav>
     </div>
   );
--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -3,7 +3,7 @@ import React from "react";
 const Link = ({ data }) => {
   const { link, name } = data;
   const linkStyle =
-    "text-white decoration-clone ml-[36px] text-xl hover:text-[#ffa500]";
+    "text-white decoration-clone ml-[36px] mb-4 lg:mb-0 border-b last:border-0 lg:border-none text-xl hover:text-[#ffa500]";
   return (
     <a className={linkStyle} href={link}>
       {name}
