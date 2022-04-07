71fe3dc7ce105d9de35dc0ee51c61e4d3722364b
tanveer <mdtanviralamht731@gmail.com>
2022-04-07 20:04:31 +0600

    search result added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
 import { addToDb } from "../../utilities/fakedb";
@@ -13,6 +13,7 @@ import { useNavigate } from "react-router-dom";
 const Shop = () => {
   // all states
   const [products, setProducts, loading, setLoading] = useProducts();
+  const [search, setSearch] = useState([]);
   // const [] = useProducts();
   const [cart, setCart, clearCart] = useCart(products);
   // use navigate path
@@ -35,36 +36,56 @@ const Shop = () => {
     setCart(newCart);
     addToDb(selectProduct.id);
   };
-  return (
-    <div className="shop-container ">
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="products-items mt-12 ml-12 md:m-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 ">
-          {products.map((product) => (
-            <Product
-              product={product}
-              handleBtn={handleAddToCartBtn}
-              key={product.id}
-            />
-          ))}
-        </div>
-      )}
+  // search result get function
+  const handleSearchResult = (event) => {
+    const searchValue = event.target.value;
+    const convertValue = searchValue.toLowerCase();
+    const match = products.filter((searchProduct) =>
+      searchProduct.name.toLowerCase().includes(searchValue)
+    );
+    setSearch(match);
+  };
 
-      <div className="product-cart">
-        <Cart cart={cart} clearCart={clearCart}>
-          {cart.length > 0 && (
-            <button
-              onClick={() => navigate("/order")}
-              className="bg-orange-500 p-2 flex w-full my-2 justify-between items-center rounded text-white"
-            >
-              Review Order
-              <FontAwesomeIcon icon={faLongArrowAltRight} />
-            </button>
-          )}
-        </Cart>
+  return (
+    <>
+      <div className="search-field my-8 text-center ">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="text-start border-2 border-blue-900 py-2 px-4 rounded w-1/3 "
+          onChange={handleSearchResult}
+        />
       </div>
-    </div>
+      <div className="shop-container ">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="products-items mt-12 ml-12 md:m-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 ">
+            {search.map((product) => (
+              <Product
+                product={product}
+                handleBtn={handleAddToCartBtn}
+                key={product.id}
+              />
+            ))}
+          </div>
+        )}
+
+        <div className="product-cart">
+          <Cart cart={cart} clearCart={clearCart}>
+            {cart.length > 0 && (
+              <button
+                onClick={() => navigate("/order")}
+                className="bg-orange-500 p-2 flex w-full my-2 justify-between items-center rounded text-white"
+              >
+                Review Order
+                <FontAwesomeIcon icon={faLongArrowAltRight} />
+              </button>
+            )}
+          </Cart>
+        </div>
+      </div>
+    </>
   );
 };
 
